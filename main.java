5bcd29da3c24be99e1e926e624665094dd5d525e
yash0305 <yashsharma199635@gmail.com>
2024-05-15 12:46:48 +0530

    Initial commit

---
+++ main.java
@@ -0,0 +1,13 @@
+import java.util.Date;
+
+public class main {
+
+    public static void main(String[] args) {
+    
+        System.out.println("This is java program");
+        System.out.println("output from java program");
+        System.out.println(new Date());
+    
+        
+    }
+}
